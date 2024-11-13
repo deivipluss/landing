f48c47b0b792839855df10ca467eb28112f675e3
Deivis Contreras Cardenas <deivipluss@gmail.com>
2024-11-13 15:45:01 +0000

    miercoles13

--- app/page.tsx
+++ app/page.tsx
@@ -73,9 +73,9 @@ const ProfessionalInfo: React.FC = () => (
 );
 
 const ActionCards: React.FC = () => (
-  <div style={{ maxWidth: '1300px' }} className="w-full px-4 md:px-0 mt-8">
-    <div className="flex flex-col md:flex-row gap-6">
-      <div className="bg-red-500 text-white p-6 md:p-8 rounded-lg shadow-md h-[100px] md:h-[240px] flex items-center md:items-start w-full md:flex-1">
+  <div className="w-full !max-w-[1300px] px-4 md:px-0 mt-8">
+    <div className="flex flex-col md:flex-row gap-4 md:gap-6 w-full">
+    <div className="bg-red-500 text-white p-4 rounded-lg shadow-md h-[80px] md:h-[220px] w-full md:w-[48%]">
         <div className="flex items-center md:items-start space-x-3">
           <RiCustomerService2Line className="text-2xl md:text-3xl" />
           <h3 className="text-lg md:text-xl font-semibold">SOLUCIONES</h3>
@@ -85,7 +85,7 @@ const ActionCards: React.FC = () => (
         </div>
       </div>
 
-      <div className="bg-green-500 text-white p-6 md:p-8 rounded-lg shadow-md h-[100px] md:h-[240px] flex items-center md:items-start w-full md:flex-1">
+      <div className="bg-green-500 text-white p-4 rounded-lg shadow-md h-[80px] md:h-[220px] w-full md:w-[48%]">
         <div className="flex items-center md:items-start space-x-3">
           <FaWhatsapp className="text-2xl md:text-3xl" />
           <h3 className="text-lg md:text-xl font-semibold">CONTACTA CONMIGO</h3>
