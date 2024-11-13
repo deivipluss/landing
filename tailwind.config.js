8640eb17d013926f010e7438aeaca9d04c1dd8ec
Deivis Contreras Cardenas <deivipluss@gmail.com>
2024-11-13 16:00:36 +0000

    miercoles13

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,20 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#FF5C5C',
+        'secondary': '#4A90E2',
+        'tertiary': '#0D0C1D',
+        'lightGray': '#E1E1E1',
+      },
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+      },
+      screens: {
+        'xs': '480px',  // Definir tamaño extra pequeño
+      },
+    },
   },
   plugins: [],
 };
