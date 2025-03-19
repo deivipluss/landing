93aa65845111b109b45bd411923d414539bb0b46
Deivis Contreras Cárdenas <deivipluss@gmail.com>
2025-03-19 15:54:11 +0000

    contract

--- app/api/save-to-sheets/route.ts
+++ app/api/save-to-sheets/route.ts
@@ -3,7 +3,7 @@ import { NextRequest, NextResponse } from 'next/server';
 // Este endpoint está en desuso porque estamos usando directamente Google Sheets
 // Se mantiene como referencia para posible uso futuro
 
-export async function POST(req: NextRequest) {
+export async function POST(_req: NextRequest) {
   return NextResponse.json({ message: "Este endpoint está deshabilitado. Utilizamos Google Sheets directamente." }, { status: 200 });
 }
 
