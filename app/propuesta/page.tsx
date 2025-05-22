923d68fb51c65800eebc49abf5c81a9c271f2773
Deivis Contreras Cárdenas <deivipluss@gmail.com>
2025-05-22 20:41:41 +0000

    propuesta

--- app/propuesta/page.tsx
+++ app/propuesta/page.tsx
@@ -1,7 +1,7 @@
 // Create a Next.js page that renders a full-screen vertical scroll presentation using slides
 // Each slide should be full height, centered, and have snap scrolling
 // Import slides from a data file
-import { HorizontalSlides, proposalSlides } from "../../data/propuestaContent.tsx";
+import { HorizontalSlides, proposalSlides } from "../../data/propuestaContent";
 
 export default function PropuestaPage() {
   return (
